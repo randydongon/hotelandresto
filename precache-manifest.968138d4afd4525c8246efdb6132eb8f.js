2ed60dc680c8bb6851e9bc74227edcfaba33985c
randydongon <bodrorandy@gmail.com>
2022-02-02 23:40:40 +0800

    Updates

---
+++ precache-manifest.968138d4afd4525c8246efdb6132eb8f.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d399ccb9e06d7640cb231e39eb97e959",
+    "url": "/hotelandresto/index.html"
+  },
+  {
+    "revision": "55f1e93ea7b364172958",
+    "url": "/hotelandresto/static/css/2.d98961f1.chunk.css"
+  },
+  {
+    "revision": "fc29cd50a85917fa3681",
+    "url": "/hotelandresto/static/css/main.d9a53baa.chunk.css"
+  },
+  {
+    "revision": "55f1e93ea7b364172958",
+    "url": "/hotelandresto/static/js/2.44d82783.chunk.js"
+  },
+  {
+    "revision": "5403b55c1cb422a72be68a013c0b9049",
+    "url": "/hotelandresto/static/js/2.44d82783.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc29cd50a85917fa3681",
+    "url": "/hotelandresto/static/js/main.2c67884d.chunk.js"
+  },
+  {
+    "revision": "a0a6cafa78f615f0c2dc",
+    "url": "/hotelandresto/static/js/runtime-main.0ac1d7a7.js"
+  },
+  {
+    "revision": "c6ed81dd7b049ea2040f1300cb78060a",
+    "url": "/hotelandresto/static/media/about.c6ed81dd.svg"
+  },
+  {
+    "revision": "e65a6536fd277930d0d7eeccd7dfb89f",
+    "url": "/hotelandresto/static/media/customer.e65a6536.svg"
+  },
+  {
+    "revision": "f6f05af4ebfd528b999e303df5a90263",
+    "url": "/hotelandresto/static/media/customer1.f6f05af4.svg"
+  },
+  {
+    "revision": "9dac55866191ecfd805f015323ccf807",
+    "url": "/hotelandresto/static/media/customer3.9dac5586.svg"
+  },
+  {
+    "revision": "3f58ff21a31cb4ddc42995ddb45a1509",
+    "url": "/hotelandresto/static/media/customer4.3f58ff21.svg"
+  },
+  {
+    "revision": "4072dfe37b2a8ba5e0b421c8982c4f30",
+    "url": "/hotelandresto/static/media/defaultBcg.4072dfe3.jpg"
+  },
+  {
+    "revision": "33ab9f1e88f204b5b8a13725a918f36c",
+    "url": "/hotelandresto/static/media/details-1.33ab9f1e.jpeg"
+  },
+  {
+    "revision": "50c23e6265e40d602f1ac080e2d4169a",
+    "url": "/hotelandresto/static/media/details-2.50c23e62.jpeg"
+  },
+  {
+    "revision": "7345b9f61680c799868f66f297cedab5",
+    "url": "/hotelandresto/static/media/details-3.7345b9f6.jpeg"
+  },
+  {
+    "revision": "7569a31606be3db6b229327eec5999df",
+    "url": "/hotelandresto/static/media/details-4.7569a316.jpeg"
+  },
+  {
+    "revision": "6edb36f3feccea6f8483d2808828b6e1",
+    "url": "/hotelandresto/static/media/error.6edb36f3.jpg"
+  },
+  {
+    "revision": "e9ea73949de71d45ae46047646b95727",
+    "url": "/hotelandresto/static/media/loading-arrow.e9ea7394.gif"
+  },
+  {
+    "revision": "8c3d2a79d2974762d90310ec87bec6e6",
+    "url": "/hotelandresto/static/media/notfound.8c3d2a79.svg"
+  },
+  {
+    "revision": "e928a5c462a9f4f590af64285b387571",
+    "url": "/hotelandresto/static/media/room-1.e928a5c4.jpeg"
+  },
+  {
+    "revision": "3b5c08716be6b713154c8aedaec3f5bf",
+    "url": "/hotelandresto/static/media/room-10.3b5c0871.jpeg"
+  },
+  {
+    "revision": "f08fee37bdc8f3d11ef36658cfacc44e",
+    "url": "/hotelandresto/static/media/room-11.f08fee37.jpeg"
+  },
+  {
+    "revision": "0e293352532d9483616fe4c5110c8e28",
+    "url": "/hotelandresto/static/media/room-12.0e293352.jpeg"
+  },
+  {
+    "revision": "052c76815f889f20b8a498c19a212c50",
+    "url": "/hotelandresto/static/media/room-2.052c7681.jpeg"
+  },
+  {
+    "revision": "11343adfcff21648fa4c3128e13f88aa",
+    "url": "/hotelandresto/static/media/room-2.11343adf.jpg"
+  },
+  {
+    "revision": "9a1103b597895b4c4b373d6304400781",
+    "url": "/hotelandresto/static/media/room-3.9a1103b5.jpeg"
+  },
+  {
+    "revision": "1e407d88a352a0e8493e2275a76f39f5",
+    "url": "/hotelandresto/static/media/room-4.1e407d88.jpeg"
+  },
+  {
+    "revision": "a5c06e0bb3e90f927159439d68b07253",
+    "url": "/hotelandresto/static/media/room-5.a5c06e0b.jpeg"
+  },
+  {
+    "revision": "b9a34e969ab86c1500d67183f646995a",
+    "url": "/hotelandresto/static/media/room-6.b9a34e96.jpeg"
+  },
+  {
+    "revision": "e251c8d571a7f753b3da985e4b8a34c6",
+    "url": "/hotelandresto/static/media/room-7.e251c8d5.jpeg"
+  },
+  {
+    "revision": "ebb7fca5f456fb07e0096ab3ef72b330",
+    "url": "/hotelandresto/static/media/room-8.ebb7fca5.jpeg"
+  },
+  {
+    "revision": "7b17c49f01aa17fc11df8400d604e1c9",
+    "url": "/hotelandresto/static/media/room-9.7b17c49f.jpeg"
+  }
+]);
